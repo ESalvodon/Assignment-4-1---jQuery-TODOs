a564bbf4e0dcd15085ee2fb393dec4708bbdfdd7
Eddy Salvodon <Esalvodon@Gmail.com>
2016-01-07 18:04:51 -0500

    2nd commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,23 +6,16 @@ $(document).ready(function() {
     } else {
       $(this).parent().find('.text').css('text-decoration', 'none');
     }
-  }
+  };
   $('.checkbox').click(mark_done);
+
+
   $('#addNew').click(function(){
-    //var row= '<input  type=text name=todo />'
     var newTask = $('#userInput').val();
     $("ul:last").append('<li>' + '<div class = "newTask">' + '<input class="checkbox" type="checkbox">' +
-                       '<input class="text" type="text" name="todo" value="' + newTask + '">' + '</div></li>');
+    '<input class="text" type="text" name="todo" value="' + newTask + '">' + '</div></li>');
     $('.checkbox').last().click(mark_done);
   });
-
-  // $('#userInput').on('click', 'input.checkbox' ,function (event) {
-  //   if (true) {
-  //   $(this).css('text-decoration', 'line-through');
-
-  // }
-
-  // });
 
 
 
